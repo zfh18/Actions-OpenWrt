b36a0724db9d1dd7ab3d398dbe6eaf85697905b2
tom.zhou <1650535970@qq.com>
2020-01-31 16:01:34 +0800

    Change default theme

--- diy.sh
+++ diy.sh
@@ -7,4 +7,8 @@
 #=================================================
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.50.20/g' package/base-files/files/bin/config_generate
+#Modify default theme
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
+sed -i 's/Bootstrap/Argon/g' feeds/luci/collections/luci/Makefile
+#Build OpenClash
 git clone https://github.com/vernesong/OpenClash.git package/OpenClash
