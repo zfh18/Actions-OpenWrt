77c840cd7e12936da4b8e696b07cb87591954be8
tom.zhou <1650535970@qq.com>
2020-02-22 13:13:26 +0800

    Add build adguardhome

--- diy.sh
+++ diy.sh
@@ -12,5 +12,7 @@ sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/M
 sed -i 's/Bootstrap/Argon/g' feeds/luci/collections/luci/Makefile
 #Build OpenClash
 git clone https://github.com/vernesong/OpenClash.git package/OpenClash
+# Build AdguardHome
+git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/adguardhome
 #Build SmartDNS
 #git clone https://github.com/pymumu/smartdns.git package/smartdns
