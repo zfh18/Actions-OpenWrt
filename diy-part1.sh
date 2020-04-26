fbd872473220da1224d912699027f8a3bda3ec5b
tom.zhou <1650535970@qq.com>
2020-04-26 16:02:28 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,15 +1,23 @@
 #!/bin/bash
-#=============================================================
-# https://github.com/P3TERX/Actions-OpenWrt
-# File name: diy-part1.sh
-# Description: OpenWrt DIY script part 1 (Before Update feeds)
+#=================================================
+# Description: DIY script
 # Lisence: MIT
 # Author: P3TERX
 # Blog: https://p3terx.com
-#=============================================================
-
-# fw876/helloworld
-#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
-
-# Lienol/openwrt-package
-#sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+#=================================================
+# Modify default IP
+sed -i 's/192.168.1.1/192.168.50.20/g' package/base-files/files/bin/config_generate
+#Update Argon and modify default theme
+cd package/lean
+rm -rf luci-theme-argon
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git
+cd ..
+cd ..
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
+sed -i 's/Bootstrap/Argon/g' feeds/luci/collections/luci/Makefile
+#Build OpenClash
+git clone https://github.com/vernesong/OpenClash.git package/OpenClash
+# Build AdguardHome
+git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/adguardhome
+#Build SmartDNS
+#git clone https://github.com/pymumu/smartdns.git package/smartdns
