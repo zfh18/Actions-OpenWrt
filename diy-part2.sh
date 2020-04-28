27c51967cde0ebd5dd0e461ca93f1d2f67d92bd7
tom.zhou <1650535970@qq.com>
2020-04-28 21:32:47 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,12 +1,23 @@
 #!/bin/bash
-#============================================================
-# https://github.com/P3TERX/Actions-OpenWrt
-# File name: diy-part2.sh
-# Description: OpenWrt DIY script part 2 (After Update feeds)
+#=================================================
+# Description: DIY script
 # Lisence: MIT
 # Author: P3TERX
 # Blog: https://p3terx.com
-#============================================================
-
+#=================================================
 # Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.50.20/g' package/base-files/files/bin/config_generate
+#Update Argon and modify default theme
+cd package/lean
+rm -rf luci-theme-argon
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git
+cd ..
+cd ..
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
+sed -i 's/Bootstrap/Argon/g' feeds/luci/collections/luci/Makefile
+#Build OpenClash
+git clone https://github.com/vernesong/OpenClash.git package/OpenClash
+# Build AdguardHome
+git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/adguardhome
+#Build SmartDNS
+#git clone https://github.com/pymumu/smartdns.git package/smartdns
