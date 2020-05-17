1935f353bdc6e7d774834431595c29a39c74b910
tom.zhou <1650535970@qq.com>
2020-05-17 15:32:52 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -6,7 +6,7 @@
 # Blog: https://p3terx.com
 #=================================================
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.50.21/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.50.20/g' package/base-files/files/bin/config_generate
 #Update Argon and modify default theme
 cd package/lean
 rm -rf luci-theme-argon
